9a9bf4562d9c8eac6950f37e8b37164c0df31075
admin <tunguyen090896@gmail.com>
2019-05-14 14:22:08 +0700

    Initial Commit

---
+++ ObjectiveCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveCDemo
+//
+//  Created by admin on 5/14/19.
+//  Copyright © 2019 com.dinhtu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveCDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ObjectiveCDemo
+//
+//  Created by admin on 5/14/19.
+//  Copyright © 2019 com.dinhtu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
